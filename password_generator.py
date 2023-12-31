f5263c0cfa9338fb1f823ffb466a71a963cb08c8
Stormizn <ayushistakenmc@gmail.com>
2023-12-31 18:14:32 +0530

    Fixed grammatical errors

--- password_generator.py
+++ password_generator.py
@@ -1,6 +1,6 @@
 import random
 
-lenght=random.randint(8, 10)
+length=random.randint(8, 10)
 
 capital_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 small_letters = 'abcdefghijklmnopqrstuvwxyz'
@@ -8,7 +8,7 @@ numbers = '0123456789'
 special_characters = '!@#$%^&*()-+'
 all = capital_letters + small_letters + numbers + special_characters
 
-temp=random.sample(all, lenght)
+temp=random.sample(all, length)
 
 password="".join(temp)
 
